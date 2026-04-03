9dd4d9fcc39977fd6cfeff2b244ea43c495c3515
gopanm <gopanmangalassery369@gmail.com>
2026-04-03 10:53:27 +0530

    Add Selenium test for Google homepage title
    Replaced testPass method with testGoogleTitle method that tests Google homepage title using Selenium WebDriver.

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -1,11 +1,22 @@
-import org.testng.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class TestExample {
 
     @Test
-    public void testPass() {
-        System.out.println("Test is running...");
-        Assert.assertTrue(true);
+    public void testGoogleTitle() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+
+        System.out.println("Title: " + driver.getTitle());
+
+        driver.quit();
     }
 }
